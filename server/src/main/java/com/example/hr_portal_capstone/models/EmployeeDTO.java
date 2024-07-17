476743345a8bbc227ab32fe5d98a4d88d3009599
Ghishani Mohanadas <ghishani@hotmail.co.uk>
2024-07-17 17:34:36 +0100

    completed EmployeeDTO

--- server/src/main/java/com/example/hr_portal_capstone/models/EmployeeDTO.java
+++ server/src/main/java/com/example/hr_portal_capstone/models/EmployeeDTO.java
@@ -1,4 +1,51 @@
 package com.example.hr_portal_capstone.models;
 
 public class EmployeeDTO {
+
+    private String firstName;
+    private String lastName;
+    private int teamId;
+    private Grade grade;
+
+    public EmployeeDTO(String firstName, String lastName, int teamId, Grade grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.teamId = teamId;
+        this.grade = grade;
+    }
+
+    public EmployeeDTO() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
 }
