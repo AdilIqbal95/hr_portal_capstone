3f464868171cf23e74de177374043d7521c7c0a4
Ghishani Mohanadas <ghishani@hotmail.co.uk>
2024-07-17 16:07:12 +0100

    initialised server

---
+++ server/src/main/java/com/example/hr_portal_capstone/HrPortalCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.hr_portal_capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPortalCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPortalCapstoneApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/hr_portal_capstone/HrPortalCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hr_portal_capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPortalCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
