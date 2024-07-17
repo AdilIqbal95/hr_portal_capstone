943e6dbefeea2182d392416f25d7a15727d87ded
Dena Shiranipour <denashirani@gmail.com>
2024-07-17 17:28:36 +0100

    Completed the HolidayDTO class

--- server/src/main/java/com/example/hr_portal_capstone/models/HolidayDTO.java
+++ server/src/main/java/com/example/hr_portal_capstone/models/HolidayDTO.java
@@ -1,4 +1,68 @@
 package com.example.hr_portal_capstone.models;
 
+import java.time.LocalDate;
+
 public class HolidayDTO {
+
+    private int employeeId;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private Reason reason;
+
+    private Status status;
+
+
+    public HolidayDTO(int employeeId, LocalDate startDate, LocalDate endDate, Reason reason, Status status) {
+        this.employeeId = employeeId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.reason = reason;
+        this.status = status;
+    }
+
+    public HolidayDTO() {
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public Reason getReason() {
+        return reason;
+    }
+
+    public void setReason(Reason reason) {
+        this.reason = reason;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
