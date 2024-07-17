af67b5f1d9f464ae3ca0052ac128d7698cb05a4e
adiliqbal95 <adil.iqbal@hotmail.co.uk>
2024-07-17 17:39:13 +0100

    added placeholder for user profile

---
+++ frontend/src/components/UserProfile.jsx
@@ -0,0 +1,23 @@
+
+function UserProfile() {
+
+    return (
+      <>
+        <section>
+            <article>
+                <img src="" alt="Image of user" />
+            </article>
+            <article>
+                <p>Placeholder name</p>
+                <p>Placeholder email</p>
+                <p>Placeholder team name</p>
+                <p>Placeholder location</p>
+            </article>
+        </section>
+        
+
+      </>
+    )
+  }
+  
+  export default UserProfile;
