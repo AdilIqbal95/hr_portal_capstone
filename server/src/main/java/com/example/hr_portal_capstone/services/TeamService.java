1a3bb99cdfddcd20bbf03be4240e9225f5b2fce1
Sabah Yusuf <sabah.yusuf@outlook.com>
2024-07-18 18:16:59 +0100

    team service and two routes to  controller

--- server/src/main/java/com/example/hr_portal_capstone/services/TeamService.java
+++ server/src/main/java/com/example/hr_portal_capstone/services/TeamService.java
@@ -1,7 +1,30 @@
 package com.example.hr_portal_capstone.services;
 
+import com.example.hr_portal_capstone.models.Team;
+import com.example.hr_portal_capstone.repositories.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TeamService {
+
+    @Autowired
+    TeamRepository teamRepository;
+
+
+    public List<Team> getAllTeamsById(){
+        return teamRepository.findAll();
+    }
+
+
+    public Optional<Team> getTeamById(long id) {
+        return teamRepository.findById(id);
+    }
+
+
+
+
 }
