25a16a7033bb27a8796be9be41f51c8114c43a45
maria <mariasharif.16@hotmail.com>
2024-07-18 11:40:22 +0100

    addition of methods to our employee serivces and changes to our employee controller

--- server/src/main/java/com/example/hr_portal_capstone/controllers/EmployeeController.java
+++ server/src/main/java/com/example/hr_portal_capstone/controllers/EmployeeController.java
@@ -1,4 +1,47 @@
 package com.example.hr_portal_capstone.controllers;
 
+import com.example.hr_portal_capstone.models.Employee;
+import com.example.hr_portal_capstone.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployee(){
+        List<Employee> employees = employeeService.getAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping ("/{id}")
+    public ResponseEntity<Optional<Employee>> getEmployeeById(@PathVariable long id){
+        Optional<Employee> employee = employeeService.getEmployeeById(id);
+        if(employee.isPresent()){
+            return new ResponseEntity<>(employee, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){
+        Employee savedEmployee = employeeService.createEmployee(employee);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping (value = "{id}")
+    public ResponseEntity<Void> deleteEmployeeById(@PathVariable long id){
+        employeeService.deleteEmployeeById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
 }
--- server/src/main/java/com/example/hr_portal_capstone/services/EmployeeService.java
+++ server/src/main/java/com/example/hr_portal_capstone/services/EmployeeService.java
@@ -1,4 +1,32 @@
 package com.example.hr_portal_capstone.services;
 
+import com.example.hr_portal_capstone.models.Employee;
+import com.example.hr_portal_capstone.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
 public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployeeById(long id) {
+        Employee employee = this.getEmployeeById(id).get();
+            this.employeeRepository.delete(employee);
+    }
+
 }
