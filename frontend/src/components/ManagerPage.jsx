5118558bcdf85f7cc330400ad50f7f2347f9cac2
Ghishani Mohanadas <ghishani@hotmail.co.uk>
2024-07-18 12:57:12 +0100

    added EmployeeForm and HolidayApprovalList to ManagerPage

--- frontend/src/components/ManagerPage.jsx
+++ frontend/src/components/ManagerPage.jsx
@@ -1,10 +1,14 @@
-import UserProfile from "./UserProfile";
+import EmployeeForm from "./EmployeeForm";
+import HolidayApprovalList from "./HolidayApprovalList";
 
 function ManagerPage() {
 
     return (
       <>
         <h1>Manager Page</h1>
+        <EmployeeForm />
+        <HolidayApprovalList />
+        
 
       </>
     )
