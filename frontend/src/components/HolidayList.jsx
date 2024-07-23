49b42e8c0a9e2f3f97c2ddc38fb94322d9ab28c3
Dena Shiranipour <denashirani@gmail.com>
2024-07-23 12:17:58 +0100

    created a function to modify status in the Holiday List

--- frontend/src/components/HolidayList.jsx
+++ frontend/src/components/HolidayList.jsx
@@ -27,18 +27,25 @@ const HolidayList = ({currentUser}) => {
 
 
 const handleReason = (reason) => {
-const reasonWithoutUnderscore = reason.split('_').join(' ')
+  const reasonWithoutUnderscore = reason.split('_').join(' ')
 
-const reasonWithoutUnderscoreLower = reasonWithoutUnderscore.toLowerCase().split(' ');
+  const reasonWithoutUnderscoreLower = reasonWithoutUnderscore.toLowerCase().split(' ');
 
-for (let i = 0; i < reasonWithoutUnderscoreLower.length; i++){
-  reasonWithoutUnderscoreLower[i] = reasonWithoutUnderscoreLower[i][0].toUpperCase() + reasonWithoutUnderscoreLower[i].substr(1);
+  for (let i = 0; i < reasonWithoutUnderscoreLower.length; i++){
+    reasonWithoutUnderscoreLower[i] = reasonWithoutUnderscoreLower[i][0].toUpperCase() + reasonWithoutUnderscoreLower[i].substr(1);
+  }
+
+    const reasonWithoutUnderscoreStr = reasonWithoutUnderscoreLower.join(' ')
+    return reasonWithoutUnderscoreStr;
 }
 
-  const reasonWithoutUnderscoreStr = reasonWithoutUnderscoreLower.join(' ')
-  return reasonWithoutUnderscoreStr;
-}
 
+const handleStatus = (status) => {
+  const statusLower = status.toLowerCase();
+  const finalStatus = statusLower[0].toUpperCase() + statusLower.substr(1);
+
+  return finalStatus;
+}
 
 
 
@@ -50,7 +57,7 @@ const mappedHolidayList = currentUser.holidays.map((holiday) => {
         <p> <strong> Start Date: </strong> {new Date(holiday.startDate).toLocaleDateString()}</p>
         <p> <strong> End Date: </strong> {new Date(holiday.endDate).toLocaleDateString()}</p>
         <p> <strong> Holiday Reason: </strong>{handleReason(holiday.reason)}</p>
-        <p> <strong> Status: </strong>{holiday.status}</p>
+        <p> <strong> Status: </strong>{handleStatus(holiday.status)}</p>
       </article>
       <article>
         <button>Update</button>
